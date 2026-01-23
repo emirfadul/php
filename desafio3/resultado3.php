b476e06ab91dfd158ecb4f25605d613d49477ce5
emirfadul <emirfadul@gmail.com>
2026-01-23 17:35:01 -0400

    atualizado em 23.jan as 15h34

--- desafio3/resultado3.php
+++ desafio3/resultado3.php
@@ -5,28 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desafio 4</title>
-    <link rel="stylesheet" href="/desafio3/style.css">
+    <link rel="stylesheet" href="/php/desafio3/style.css">
 </head>
 
 <body>
     <main>
         <h1>Conversão de Real para Dolar</h1>
         <?php
-            $reais = $_GET['reais'] ?? 0;
-            $inicio = date("m-d-Y", strtotime("-7 days"));
-            $fim = date("m-d-Y");
-
-            $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\'' . $inicio . '\'&@dataFinalCotacao=\'' . $fim . '\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
-
-            $dados = json_decode(file_get_contents($url), true);
-        
-            if ($dados && isset($dados["value"][0]["cotacaoCompra"])) {
-                $cotacaoCompra = $dados["value"][0]["cotacaoCompra"];
-                $dolares = $reais / $cotacaoCompra;
-                echo "<p>Com R$ " . number_format($reais, 2, ',', '.') . " você pode comprar US$ " . number_format($dolares, 2, '.', ',') . "</p>";
-            } else {
-                echo "<p>Erro ao obter cotação.</p>";
-            }
+            $cotacao = $_GET['cotacao'];
+            $reais = $_GET['reais'];
+            $dolar = $reais / $cotacao;
+            echo "<p>Com R$ " . number_format($reais, 2, ',', '.') . " você pode comprar US$ " . number_format($dolar, 2, '.', ',') . "</p>";
         ?>
 
         <button onclick="javascript:history.go(-1)">&#x2B05; Voltar</button>
--- desafio4/conversao.php
+++ desafio4/conversao.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desafio 4</title>
-    <link rel="stylesheet" href="/desafio4/style.css">
+    <link rel="stylesheet" href="/php/desafio4/style.css">
 </head>
 
 <body>
@@ -14,20 +14,18 @@
         <?php
             $reais = $_REQUEST['reais'] ?? 0;
             $inicio = date("m-d-Y", strtotime("-7 days"));
-            $fim = date("m-d-Y");
+            $fim = date("(m-d-Y");
 
-            $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\'' . 
-            $inicio . '\'&@dataFinalCotacao=\'' . $fim . '\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+            $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=' . $inicio . '&@dataFinalCotacao=' . $fim . '&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
 
             $dados = json_decode(file_get_contents($url), true);
-            
-            if ($dados && isset($dados["value"][0]["cotacaoCompra"])) {
-                $cotacao = $dados["value"][0]["cotacaoCompra"];
-                $dolar = $reais / $cotacao;
-                echo "<p>Com R$ " . number_format($reais, 2, ',', '.') . " você pode comprar US$ " . number_format($dolar, 2, '.', ',') . "</p>";
-            } else {
-                echo "<p>Erro ao obter cotação.</p>";
-            }
+
+            //var_dump($dados); 
+
+            $cotacao = $dados["value"][0]["codtacaoCompra"];
+
+            echo "A cotaçáo foi de R$ " . $cotacao;
+         
         ?>
 
         <button onclick="javascript:history.go(-1)">&#x2B05; Voltar</button>
