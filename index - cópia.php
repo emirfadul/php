03b32f78e23c89a8c4d2cd974063ae81980e60f8
emirfadul <emirfadul@gmail.com>
2026-02-03 15:29:17 -0400

    alterei os arquivos php

---
+++ index - cópia.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <h1>Curso PHP - Gustavo Guanabara</h1>
+    </header>
+
+    <div class="cards-container">
+
+        <!-- Card Individual -->
+        <div class="card">
+            <img src="/logo_php.png" alt="Descrição da imagem" class="card-image">
+
+            <div class="card-content">
+                <a href="/desafio10/index.php">
+                    <button class="card-button">DESAFIO 10</button>
+                </a>                
+            </div>
+            <p>Calculando sua idade.</p>
+        </div>
+
+        <!-- Card Individual -->
+        <div class="card">
+            <img src="/logo_php.png" alt="Descrição da imagem" class="card-image">
+
+            <div class="card-content">
+                <a href="desafio11/index.php">
+                    <button class="card-button">DESAFIO 11</button>
+                </a>                
+            </div>
+            <p>Reajuste de preço.</p>
+        </div>
+
+        <!-- Card Individual -->
+        <div class="card">
+            <img src="/logo_php.png" alt="Descrição da imagem" class="card-image">
+
+            <div class="card-content">
+                <a href="desafio12/index.php">
+                    <button class="card-button">DESAFIO 12</button>
+                </a>                
+            </div>
+            <p>Calculando de Tempo.</p>
+        </div>
+
+        <div class="card">
+            <img src="/logo_php.png" alt="Descrição da imagem" class="card-image">
+
+            <div class="card-content">
+                <a href="desafio13/index.php">
+                    <button class="card-button">DESAFIO 13</button>
+                </a>                
+            </div>
+            <p>Calculando sua idade.</p>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
