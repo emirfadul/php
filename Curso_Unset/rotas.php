f40e5c72e8168a9d71ab35ad8ac5b67232b80ebc
emirfadul <emirfadul@gmail.com>
2026-03-03 17:44:24 -0400

    trabalhando com modulos

--- Curso_Unset/rotas.php
+++ Curso_Unset/rotas.php
@@ -4,8 +4,8 @@ use Pecee\SimpleRouter\SimpleRouter;
 
 SimpleRouter::setDefaultNamespace(defaultNamespace: 'Sistema\Controlador');
 
-SimpleRouter::get('/',  'SiteControlador@index');
-SimpleRouter::get('/sobre',  'SiteControlador@sobre');
+SimpleRouter::get(URL_SITE,  'SiteControlador@index');
+SimpleRouter::get(URL_SITE . 'sobre', 'SiteControlador@sobre');
 
 SimpleRouter::start();
 
